088ddfae041bd8c9d48ac892da60575d70595b99
uche-inyama <depoulet@yahoo.com>
2022-03-27 16:41:02 +0000

    setup devise to work with hotwire

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  authenticated do
+    root to: "events#index", as: :authenticated_root
+  end
   root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_152238) do
+ActiveRecord::Schema.define(version: 2022_03_27_160349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.time "time"
+    t.text "address"
+    t.text "theme"
+    t.text "details"
+    t.text "image_data"
+    t.string "event_type"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +43,5 @@ ActiveRecord::Schema.define(version: 2022_03_27_152238) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
