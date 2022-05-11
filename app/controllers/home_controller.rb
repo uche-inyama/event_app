18fe2b74acaa4ce0cba861529306e0e6ec28c7fb
uche-inyama <depoulet@yahoo.com>
2022-05-11 14:39:50 +0000

    update new_event_form to show on a new page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+
+    url = 'http://jsonplaceholder.typecode.com/posts/1'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    standard = JSON.parse(response)
+    @post = JSON.parse(response, symbolize_name: true)
   end
 end
