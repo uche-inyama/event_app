6598b01d00fc5be58af494d53f6ff1c809dc7c9a
uche-inyama <depoulet@yahoo.com>
2022-05-12 12:19:28 +0000

    comment avatar and image uploader files

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,9 +1,9 @@
-require_relative '../../config/initializers/shrine'
+# require_relative '../../config/initializers/shrine'
 
-class AvatarUploader < Shrine
+# class AvatarUploader < Shrine
 
-  Attacher.validate do
-    validate_mime_type %w[image/jpeg image/png image/webp]
-    validate_max_size 1*1024*1024
-  end
-end
+#   Attacher.validate do
+#     validate_mime_type %w[image/jpeg image/png image/webp]
+#     validate_max_size 1*1024*1024
+#   end
+# end
